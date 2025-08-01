7af6fccd755569692174022d1ce7002bf1f7c219
Ксения <kssshaab@gmail.com>
2025-08-01 19:27:48 +0300

    formatting

--- src/product.py
+++ src/product.py
@@ -1,10 +1,12 @@
 from typing import List
+
 all_products: List["Product"] = []
+
 
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
@@ -31,45 +33,41 @@ class Product:
 
     @classmethod
     def new_product(cls, data: dict) -> "Product":
-        global all_products
         for pr in all_products:
             if pr.name.lower() == data["name"].lower():
                 pr.quantity += data["quantity"]
                 pr.price = data["price"]
                 return pr
-        new_product = cls(
-            data["name"],
-            data["description"],
-            data["price"],
-            data["quantity"]
-        )
+        new_product = cls(data["name"], data["description"], data["price"], data["quantity"])
         all_products.append(new_product)
         return new_product
+
 
 if __name__ == "__main__":
     product = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
     print(f"{product.name}: {product.description}, {product.price} руб., {product.quantity} шт.")
     another_product = Product("iPhone 14", "256GB, Gray space", 211000.0, 12)
-    print(f"{another_product.name}: {another_product.description}, {another_product.price}, {another_product.quantity}.")
+    print(
+        f"{another_product.name}: {another_product.description}, {another_product.price}, {another_product.quantity}."
+    )
     all_products = []
-    Product.new_product({
-    "name": "Samsung Galaxy C23 Ultra",
-    "description": "256GB, Серый цвет, 200MP камера",
-    "price": 180000.0,
-    "quantity": 5
-    })
-    Product.new_product({
-    "name": "Samsung Galaxy C23 Ultra",
-    "description": "256GB,Белый цвет, 200MP камера",
-    "price": 180000.0,
-    "quantity": 5
-    })
-    Product.new_product({
-        "name": "Iphone 15",
-        "description": "512GB, Gray space",
-        "price": 210000.0,
-        "quantity": 8
-      })
+    Product.new_product(
+        {
+            "name": "Samsung Galaxy C23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5,
+        }
+    )
+    Product.new_product(
+        {
+            "name": "Samsung Galaxy C23 Ultra",
+            "description": "256GB,Белый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5,
+        }
+    )
+    Product.new_product({"name": "Iphone 15", "description": "512GB, Gray space", "price": 210000.0, "quantity": 8})
     for product in all_products:
         print(f"{product.name}: {product.description}, {product.price} руб., {product.quantity} шт.")
     print(product.price)
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,8 @@
-from src.product import Product, all_products
+from typing import Any
 from unittest.mock import patch
+
+from src.product import Product, all_products
+
 
 def test_product(product: Product) -> None:
     assert product.name == "Samsung Galaxy C23 Ultra"
@@ -7,11 +10,12 @@ def test_product(product: Product) -> None:
     assert product.price == 180000.0
     assert product.quantity == 5
 
+
 def test_price_property(product: Product) -> None:
     assert product.price == 180000.0
 
 
-def test_price_fail(capsys):
+def test_price_fail(capsys: Any) -> None:
     product = Product("Test", "test", 1000, 1)
     product.price = 0
     captured = capsys.readouterr()
@@ -23,34 +27,27 @@ def test_price_fail(capsys):
     assert "Цена не должна быть нулевая или отрицательная" in captured.out
     assert p.price == 1000
 
+
 @patch("builtins.input", return_value="y")
-def test_price_yas(mock_input) -> None:
+def test_price_yas(mock_input: Any) -> None:
     product = Product("Test", "test", 1000, 1)
     product.price = 900
     assert product.price == 900
 
+
 @patch("builtins.input", return_value="n")
-def test_price_no(mock_input, capsys):
+def test_price_no(mock_input: Any, capsys: Any) -> None:
     product = Product("Test", "test", 1000, 1)
     product.price = 900
     captured = capsys.readouterr()
     assert "Отмена изменения цены" in captured.out
     assert product.price == 1000
 
+
 def test_new_product() -> None:
     all_products.clear()
-    Product.new_product({
-        "name": "iPhone 14",
-        "description": "test",
-        "price": 200000.0,
-        "quantity": 5
-    })
-    new_product = Product.new_product({
-        "name": "iPhone 14",
-        "description": "test",
-        "price": 210000.0,
-        "quantity": 3
-    })
+    Product.new_product({"name": "iPhone 14", "description": "test", "price": 200000.0, "quantity": 5})
+    new_product = Product.new_product({"name": "iPhone 14", "description": "test", "price": 210000.0, "quantity": 3})
     assert len(all_products) == 1
     assert new_product.quantity == 8
     assert new_product.price == 210000.0
