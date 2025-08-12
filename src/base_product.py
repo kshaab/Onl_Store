f611135cc4038760d2d0a1f5e6ac8f9f3c3f91f8
Ксения <kssshaab@gmail.com>
2025-08-12 19:28:17 +0300

    add base_product

---
+++ src/base_product.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+class BaseProduct(ABC):
+
+    @abstractmethod
+    def __init__(self) -> None:
+        pass
+
+    @property
+    @abstractmethod
+    def price(self) -> float:
+        pass
+
+    @price.setter
+    @abstractmethod
+    def price(self, value: float) -> None:
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+    @abstractmethod
+    def __add__(self, other: Any) -> Any:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def new_product(cls, *args, **kwargs) -> "BaseProduct":
+        pass
